f1f9bdfd78257825506a2271677fcfa328e4db8c
Felix Lamas <felamas@gmail.com>
2020-06-02 16:02:24 -0300

    primer parte

---
+++ Solution1/ClassPersonaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Class1
+{
+	public Class1()
+	{ 
+		class Personaje
+		{
+			public enum caracteristicas;
+			public enum 
+		}
+	}
+}
